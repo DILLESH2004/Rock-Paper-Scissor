a8dd811454fdc554ce7f3ec3656601408b61214b
BHAGYA DILLESH <109259289+DILLESH2004@users.noreply.github.com>
2024-01-27 22:56:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
-// const d = "Dillesh";
-
-// let Quantity = 0;
-// function CartQuantity(){
-//     console.log(Quantity);
-//     document.getElementById("p").innerHTML=Quantity;
-// }
-
-// function Add_to_Cart(){
-//     Quantity++;
-//     document.getElementById("p").innerHTML=Quantity;
-// }
-// function Add_to_Cart2(){
-//     Quantity += 2;
-//     document.getElementById("p").innerHTML=Quantity;
-// }
-
-// function Clear(){
-//     Quantity = 0;
-//     document.getElementById("p").innerHTML=Quantity;
-// }
-
-
-const score={
+const score=JSON.parse(localStorage.getItem('Score')) || {
     Win:0,
     Lost:0,
     Draw:0
-}
+};
+
+
 document.getElementById("W").innerHTML = score.Win;
 document.getElementById("L").innerHTML = score.Lost;
 function Random1(){
@@ -44,6 +23,7 @@ function Random1(){
     UserMove = "Rock";
 
     if(ComputerMove==UserMove){
+        score.Draw+=1;
         // document.getElementById("p1").innerHTML="Draw";
         score.Draw +=1;
         alert(`You Picked ${UserMove} Computer Picked ${ComputerMove}  Win:${score.Win} Lost : ${score.Lost} Draw: ${score.Draw}`);
@@ -55,8 +35,8 @@ function Random1(){
     }else{
         score.Win+=1;
         document.getElementById("W").innerHTML = score.Win;
-        // document.getElementById("p1").innerHTML="Player Won";
         alert(`You Picked ${UserMove} Computer Picked ${ComputerMove}  Win:${score.Win} Lost : ${score.Lost} Draw: ${score.Draw}`);
+        // document.getElementById("p1").innerHTML="Player Won";
     }
 }
 
@@ -75,20 +55,20 @@ function Random2(){
     }
     UserMove = "Paper";
 
-    if(ComputerMove==UserMove){
+    if(ComputerMove==UserMove){        
+        score.Draw+=1;
         // document.getElementById("p1").innerHTML="Draw";
-                alert(`You Picked ${UserMove} Computer Picked ${ComputerMove}  Win:${score.Win} Lost : ${score.Lost} Draw: ${score.Draw}`);
+        alert(`You Picked ${UserMove} Computer Picked ${ComputerMove}  Win:${score.Win} Lost : ${score.Lost} Draw: ${score.Draw}`);
     }else if(ComputerMove=="Scissor"){
         score.Lost +=1;
         document.getElementById("L").innerHTML = score.Lost;
         // document.getElementById("p1").innerHTML="Computer Won";
-                alert(`You Picked ${UserMove} Computer Picked ${ComputerMove}  Win:${score.Win} Lost : ${score.Lost} Draw: ${score.Draw}`);
+        alert(`You Picked ${UserMove} Computer Picked ${ComputerMove}  Win:${score.Win} Lost : ${score.Lost} Draw: ${score.Draw}`);
     }else{
         score.Win+=1;
         document.getElementById("W").innerHTML = score.Win;
         // document.getElementById("p1").innerHTML="Player Won";
-                alert(`You Picked ${UserMove} Computer Picked ${ComputerMove}  Win:${score.Win} Lost : ${score.Lost} Draw: ${score.Draw}`);
-
+        alert(`You Picked ${UserMove} Computer Picked ${ComputerMove}  Win:${score.Win} Lost : ${score.Lost} Draw: ${score.Draw}`);
     }
 }
 
@@ -108,45 +88,45 @@ function Random3(){
     UserMove = "Scissor";
 
     if(ComputerMove==UserMove){
+        score.Draw+=1;
         // document.getElementById("p1").innerHTML="Draw";
-                alert(`You Picked ${UserMove} Computer Picked ${ComputerMove}  Win:${score.Win} Lost : ${score.Lost} Draw: ${score.Draw}`);
+        alert(`You Picked ${UserMove} Computer Picked ${ComputerMove}  Win:${score.Win} Lost : ${score.Lost} Draw: ${score.Draw}`);
     }else if(ComputerMove=="Paper"){
         score.Win+=1;
         document.getElementById("W").innerHTML = score.Win;
         // document.getElementById("p1").innerHTML="Player Won";
-                alert(`You Picked ${UserMove} Computer Picked ${ComputerMove}  Win:${score.Win} Lost : ${score.Lost} Draw: ${score.Draw}`);
-
+        alert(`You Picked ${UserMove} Computer Picked ${ComputerMove}  Win:${score.Win} Lost : ${score.Lost} Draw: ${score.Draw}`);
     }else{
         score.Lost +=1;
         document.getElementById("L").innerHTML = score.Lost;
         // document.getElementById("p1").innerHTML="Computer Won";
-                alert(`You Picked ${UserMove} Computer Picked ${ComputerMove}  Win:${score.Win} Lost : ${score.Lost} Draw: ${score.Draw}`);
-
+        alert(`You Picked ${UserMove} Computer Picked ${ComputerMove}  Win:${score.Win} Lost : ${score.Lost} Draw: ${score.Draw}`);
     }
 }
 
-
-
-obj={
-    name:"dillesh",
-    age:19
-};
-console.log(obj);
-obj.name='bhagya';
-console.log(obj);
-obj.job = 'Developer';
-console.log(obj);
-
-delete obj.age;
-console.log(obj);
-
+localStorage.setItem('Score',JSON.stringify(score));
 
 function Reset(){
     score.Win=0;
     score.Lost=0;
     score.Draw=0
+    localStorage.removeItem('score')
     console.log(score.Win,score.Lost,score.Draw);
     document.getElementById('W').innerHTML = 0;
     document.getElementById('L').innerHTML = 0;
 }
 
+
+const product ={
+    name:'shirt',
+    ['delivery-time']:'5.52',
+    rating:{
+        stars:4.5,
+        count:92
+    },
+    fun: function fun(){
+        console.log("this is a function")
+    }
+}
+
+console.log(product.fun())
